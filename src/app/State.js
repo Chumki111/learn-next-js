deb43fea120be3f54629f044399057f9ea81357e
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-30 16:53:18 +0600

    add change name state component

--- src/app/State.js
+++ src/app/State.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const State = () => {
     const calculateTimeLeft = () => {
-        const difference = new Date("2024-12-31T23:59:59") - new Date();
+        const difference = new Date("2024-05-17T11:59:59") - new Date();
         return {
             days: Math.floor(difference / (1000 * 60 * 60 * 24)),
             hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
@@ -15,13 +15,25 @@ const State = () => {
 
     useEffect(() => {
         const intervalId = setInterval(() => setTimeLeft(calculateTimeLeft()), 1000);
+
+        // Cleanup the interval when the component unmounts
         return () => clearInterval(intervalId);
-    }, []);
+    }, [timeLeft]); // Add timeLeft as a dependency
+
+    const [value,setValue] = useState('next-js')
+    const changeValue = () =>{
+        setValue('javascript')
+    }
     return (
-        <div>
+        <main>
            
-            <p className='text-xl'>Countdown: {timeLeft.days} Days, {timeLeft.hours} Hours, {timeLeft.minutes} Minutes, {timeLeft.seconds} Seconds</p>
-        </div>
+           <p className='text-xl'> Countdown = {timeLeft.days}: {timeLeft.hours} : {timeLeft.minutes}:  {timeLeft.seconds}</p>
+
+          <div className='my-5'> 
+          <p>Hello {value}</p>
+            <button onClick={changeValue} className='text-white bg-black px-3 py-2'>Change Value</button>
+          </div>
+        </main>
     );
 };
 
